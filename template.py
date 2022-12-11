a52a9ee8d7c0157e48783563f083201385c6e152
Brian Barbar <brianbarbar2@gmail.com>
2022-12-11 10:02:40 +0100

    Add template

---
+++ template.py
@@ -0,0 +1,15 @@
+f = open("day0input.txt", "r")
+all_lines = f.read().split("\n")
+
+line_count = 0
+result1 = 0
+result2 = 0
+
+
+# build file system
+for line in all_lines:
+    line = line
+    line_count += 1
+
+
+print(f"result 1: {result1}, result 2: {result2}")
