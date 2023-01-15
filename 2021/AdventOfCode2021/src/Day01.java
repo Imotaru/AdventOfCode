214f20ac1d16e677301da6a8a0e04f3bbf4667db
Brian Barbar <brianbarbar2@gmail.com>
2023-01-15 06:34:01 +0100

    Clean up Day1 solution

--- 2021/AdventOfCode2021/src/Day01.java
+++ 2021/AdventOfCode2021/src/Day01.java
@@ -1,33 +1,26 @@
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class Day01 {
 
     public static int Part1() {
-        ArrayList<String> lines = Helper.ReadFile(1);
-        int lastDepth = Integer.MAX_VALUE;
-        int increaseCount = 0;
-        for (String line : lines) {
-            int depth = Integer.parseInt(line);
-            if (depth > lastDepth) {
-                increaseCount++;
-            }
-            lastDepth = depth;
-        }
-        return increaseCount;
+        return Solve(true);
     }
 
     public static int Part2() {
+        return Solve(false);
+    }
+
+    static int Solve(boolean isPartOne) {
         ArrayList<String> lines = Helper.ReadFile(1);
         int lastDepth = Integer.MAX_VALUE;
         int increaseCount = 0;
-        for (int i = 0; i < lines.size() - 2; i++) {
+        int groupSize = isPartOne ? 1 : 3;
+
+        for (int i = 0; i < lines.size() - (groupSize - 1); i++) {
             int depth = 0;
-            depth += Integer.parseInt(lines.get(i));
-            depth += Integer.parseInt(lines.get(i + 1));
-            depth += Integer.parseInt(lines.get(i + 2));
+            for (int j = 0; j < groupSize; j++) {
+                depth += Integer.parseInt(lines.get(i + j));
+            }
             if (depth > lastDepth) {
                 increaseCount++;
             }
