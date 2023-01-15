ea15d1966ef13c2affb9c927963be21963eb025a
Brian Barbar <brianbarbar2@gmail.com>
2023-01-15 03:35:09 +0100

    Commit leftover changes for day 15

--- 2022/day15.py
+++ 2022/day15.py
@@ -50,30 +50,73 @@ for beacon in beacons_in_row:
     no_beacon_indexes.remove(beacon)
 
 # part 2
-for y in range(MAX_DISTRESS_POS + 1):
-    for x in range(MAX_DISTRESS_POS + 1):
-        is_unseen = True
-        largest_diff = 0
-        for s in sensors:
-            mh = get_manhatten_dist(x, y, s[0], s[1])
-            diff = s[2] - mh
-            if diff >= 0:
-                is_unseen = False
-                break
-            elif diff < largest_diff:  # is negative, so checking if abs is larger basically, but performance matters
-                largest_diff = diff
-        if is_unseen:
-            # for s in sensors:
-            #     mh = get_manhatten_dist(x, y, s[0], s[1])
-            #     diff = s[2] - mh
-            #     if diff >= 0:
-            #         is_unseen = False
-            #         break
-                # print(f"{mh} <= {s[2]} ? diff is {mh - s[2]}")
-            print(f"({x}, {y}) is unseen, result 2 is {x * 4000000 + y}")
-        else:
-            if largest_diff < 0:
-                x += abs(largest_diff) - 1
+# todo for each sensor, check only the ones in the circle with manhatten distance + 1
+# for y in range(MAX_DISTRESS_POS + 1):
+#     for x in range(MAX_DISTRESS_POS + 1):
+#         is_unseen = True
+#         largest_diff = 0
+#         for s in sensors:
+#             mh = get_manhatten_dist(x, y, s[0], s[1])
+#             diff = s[2] - mh
+#             if diff >= 0:
+#                 is_unseen = False
+#                 break
+#             elif diff < largest_diff:  # is negative, so checking if abs is larger basically, but performance matters
+#                 largest_diff = diff
+#         if is_unseen:
+#             # for s in sensors:
+#             #     mh = get_manhatten_dist(x, y, s[0], s[1])
+#             #     diff = s[2] - mh
+#             #     if diff >= 0:
+#             #         is_unseen = False
+#             #         break
+#                 # print(f"{mh} <= {s[2]} ? diff is {mh - s[2]}")
+#             print(f"({x}, {y}) is unseen, result 2 is {x * 4000000 + y}")
+#         else:
+#             if largest_diff < 0:
+#                 x += abs(largest_diff) - 1
+
+positions_to_check = []
+
+i = 0
+for s in sensors:
+    i += 1
+    for n in range(s[2] + 1):
+        x = s[0] + s[2] + 1 - n
+        y = s[1] + n
+        x2 = s[0] - s[2] - 1 + n
+        y2 = s[1] - n
+        if 0 <= x <= MAX_DISTRESS_POS and 0 <= y <= MAX_DISTRESS_POS:
+            positions_to_check.append([x, y])
+        if 0 <= x2 <= MAX_DISTRESS_POS and 0 <= y <= MAX_DISTRESS_POS:
+            positions_to_check.append([x2, y])
+        if 0 <= x <= MAX_DISTRESS_POS and 0 <= y2 <= MAX_DISTRESS_POS:
+            positions_to_check.append([x, y2])
+        if 0 <= x2 <= MAX_DISTRESS_POS and 0 <= y2 <= MAX_DISTRESS_POS:
+            positions_to_check.append([x2, y2])
+
+    print(f"Sensor {i} / {len(sensors)} done")
+
+print(len(positions_to_check))
+
+for pos in positions_to_check:
+    is_unseen = True
+    for s in sensors:
+        mh = get_manhatten_dist(pos[0], pos[1], s[0], s[1])
+        diff = s[2] - mh
+        if diff >= 0:
+            is_unseen = False
+            break
+    if is_unseen:
+        # for s in sensors:
+        #     mh = get_manhatten_dist(x, y, s[0], s[1])
+        #     diff = s[2] - mh
+        #     if diff >= 0:
+        #         is_unseen = False
+        #         break
+            # print(f"{mh} <= {s[2]} ? diff is {mh - s[2]}")
+        print(f"({pos}) is unseen, result 2 is {pos[0] * 4000000 + pos[1]}")
+
 print(f"result 1: {len(no_beacon_indexes)}, result 2: {result2}")
 
 # 7499204000000 is too low
