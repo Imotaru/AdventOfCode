09861bc68fb182176ba1d3f08042333311fd4206
Brian Barbar <brianbarbar2@gmail.com>
2023-12-02 11:00:22 +0100

    Implement reading test input

--- 2023/Helper.cs
+++ 2023/Helper.cs
@@ -5,7 +5,12 @@ namespace AdventOfCode2023 {
     public static class Helper {
         public static string[] ReadInput(int day, bool isTest = false) {
             string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\"));
-            string filePathRelativeToProject = Path.Combine(projectRoot, day < 10 ? $"Day0{day}Input.txt" : $"Day{day}Input.txt");
+            string filePathRelativeToProject;
+            if (isTest) {
+                filePathRelativeToProject = Path.Combine(projectRoot, day < 10 ? $"Day0{day}TestInput.txt" : $"Day{day}TestInput.txt");
+            } else {
+                filePathRelativeToProject = Path.Combine(projectRoot, day < 10 ? $"Day0{day}Input.txt" : $"Day{day}Input.txt");
+            }
             return File.ReadAllLines(filePathRelativeToProject);
         }
     }
