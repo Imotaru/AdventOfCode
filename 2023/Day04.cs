92d96eb4ee00e75f061ae306e757ad39e79a8baf
Brian Barbar <brianbarbar2@gmail.com>
2023-12-04 20:43:09 +0100

    Clean up code

--- 2023/Day04.cs
+++ 2023/Day04.cs
@@ -4,60 +4,17 @@ using System.Collections.Generic;
 namespace AdventOfCode2023 {
     public static class Day04 {
         public static int Solve1() {
-            int sum = 0;
-            var input = Helper.ReadInput(4, false);
-
-            for (int i = 0; i < input.Length; i++) {
-                var line = input[i].Substring(input[i].IndexOf(':') + 2);
-                List<int> winningNumbers = new List<int>();
-                List<int> drawnNumbers = new List<int>();
-                var splits = line.Split(' ');
-                bool doneWithWinning = false;
-                Console.WriteLine(line);
-                foreach (var split in splits) {
-                    if (split.Length == 0) {
-                        continue;
-                    }
-
-                    if (split == "|") {
-                        doneWithWinning = true;
-                        continue;
-                    }
-
-                    if (doneWithWinning) {
-                        drawnNumbers.Add(int.Parse(split));
-                    } else {
-                        winningNumbers.Add(int.Parse(split));
-                    }
-                }
-
-                int wins = 0;
-                foreach (var drawnNumber in drawnNumbers) {
-                    if (winningNumbers.Contains(drawnNumber)) {
-                        wins++;
-                    }
-                }
-
-                int score = 0;
-                if (wins > 0) {
-                    score = 1;
-                }
-
-                for (int j = 0; j < wins - 1; j++) {
-                    score *= 2;
-                }
-
-                sum += score;
-            }
-            
-            return sum;
+            return Solve(true);
         }
         
         public static int Solve2() {
+            return Solve(false);
+        }
+
+        static int Solve(bool isPartOne) {
             int sum = 0;
             var input = Helper.ReadInput(4, false);
 
-            Dictionary<int, int> cardWinDict = new Dictionary<int, int>();
             int[] cardAmounts = new int[input.Length];
 
             for (int i = 0; i < input.Length; i++) {
@@ -70,7 +27,6 @@ namespace AdventOfCode2023 {
                 List<int> drawnNumbers = new List<int>();
                 var splits = line.Split(' ');
                 bool doneWithWinning = false;
-                Console.WriteLine(line);
                 foreach (var split in splits) {
                     if (split.Length == 0) {
                         continue;
@@ -94,12 +50,24 @@ namespace AdventOfCode2023 {
                         wins++;
                     }
                 }
+                
+                if (isPartOne) {
+                    int score = 0;
+                    if (wins > 0) {
+                        score = 1;
+                    }
 
-                for (int j = i + 1; j < i + wins + 1; j++) {
-                    cardAmounts[j] += cardAmounts[i];
+                    for (int j = 0; j < wins - 1; j++) {
+                        score *= 2;
+                    }
+
+                    sum += score;
+                } else {
+                    for (int j = i + 1; j < i + wins + 1; j++) {
+                        cardAmounts[j] += cardAmounts[i];
+                    }
+                    sum += cardAmounts[i];
                 }
-
-                sum += cardAmounts[i];
             }
             
             return sum;
